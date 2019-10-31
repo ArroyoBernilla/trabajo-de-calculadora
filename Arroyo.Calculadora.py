2e2d19d6fdab762b151c7ce818583e4e9f94e41e
ArroyoBernilla <56905566+ArroyoBernilla@users.noreply.github.com>
2019-10-30 19:15:45 -0500

    Add files via upload

---
+++ Arroyo.Calculadora.py
@@ -0,0 +1,358 @@
+#Calculadora nro1
+#esta calculadora decifra la densidad de un ladrillo
+
+#declaracion de variables
+masa,volumen, densidad_de_un_ladrillo=0.0,0.0,0.0
+
+#Calculadora
+masa=10
+Volumen=2
+densidad_de_un_ladrillo = (masa/Volumen)
+
+#Mostrar datos
+print("masa =", masa)
+print("Volumen =", Volumen)
+print("densidad de un ladrillo =", densidad_de_un_ladrillo)
+
+#Calculadora nro2
+#esta calculadora resuleve el trabajo mecanico
+
+#declaracion  de variables
+fuerza,masa,aceleracion=0.0,0.0,0.0
+
+#calculadora
+masa1=20
+aceleracion=3
+fuerza_de_la_polea= (masa1*aceleracion)
+
+#mostrar datos
+print("masa1= ", masa1)
+print("aceleracion= ", aceleracion)
+print("fuerza_de_la_polea= ", fuerza_de_la_polea)
+
+#calculadora nro3
+#esta calculadora calcula capacidad calorifica
+
+#declaracion de variables
+capacidad_calorifica,masa2,calor_especifico,temperatura=0.0,0.0,0.0,0.0
+
+#calculadora
+masa2=15
+calor_especifico=3
+temperatura=10
+capacidad_calorifica=(masa2*calor_especifico*temperatura)
+
+#mostrar datos
+print("masa2= ", masa2)
+print("calor especifico= ", calor_especifico)
+print("temperatura= ", temperatura)
+print("capacidad caorifica= ", capacidad_calorifica)
+
+#Calculadora nro4
+#esta calculadora calcula la distancia
+
+#declaracion de variables
+distancia,velocidad,tiempo=0.0,0.0,0.0
+
+#calculadora
+velocidad=10
+tiempo=10
+distancia_recorrida=(velocidad*tiempo)
+
+#mostrar datos
+print("velocidad= ", velocidad)
+print("tiempo= ", tiempo)
+print("distancia recorrida= ", distancia_recorrida)
+
+#calculadora nro5
+#esta calculadora calcula pa presion de un papiz sobre una tabla
+
+#declaracion de variables
+fuerza1, area_de_la_tabla,presion=0.0,0.0,0.0
+
+#calculadora
+fuerza1=25
+area_de_la_tabla=5
+presion=(fuerza1/area_de_la_tabla)
+
+#mostrar datos
+print("fuerza1= ", fuerza1)
+print("area de la tabla= ", area_de_la_tabla)
+print("presion= ", presion)
+
+#calculadora nro6
+#esta calcularora halla la la potencia de un motor
+
+#declaracion de variables
+trabajo,tiempo1,potencia_de_un_motor=0.0,0.0,0.0
+
+#calculadora
+trabajo=50
+tiempo=25
+potencia_de_un_motor=(trabajo/tiempo)
+
+#mostrar datos
+print("trabajo= ", trabajo)
+print("tiempo= ", tiempo1)
+print("potencia de un motor= ",potencia_de_un_motor)
+
+#calcuuladora nro7
+#esta calculadora halla la aceleracion
+
+#declaracion de las variables
+velocidad,tiempo,aceleracion_de_un_carro=0.0,0.0,0.0
+
+#calculadora
+velocidad=40
+tiempo=20
+aceleracion_de_un_carro=(velocidad/tiempo)
+
+#mostrar datos
+print("velocidad= ", velocidad)
+print("tiempo= ", tiempo)
+print("aceleracion de un carro= ", aceleracion_de_un_carro)
+
+#calculadora nro8
+#esta calculadora  halla energia potencial gravitacional
+
+#declaracion de variables
+masa,gravedad,altura,potencia_gravitacional=0.0,0.0,0.0,0.0
+
+#calculadora
+masa=50
+gravedad=10
+altura=2
+potencia_gravitacional=(masa*gravedad*altura)
+
+#mostrar datos
+print("masa= ", masa)
+print("gravedad= ", gravedad)
+print("altura= ", altura)
+print("potencia gravitacional ", potencia_gravitacional)
+
+#calculadora nro9
+#esta calculadora halla la fuerza centipeta
+
+#declaracion de variables
+masa,aceleracion_centripeta,resultante_centripeta=0.0,0.0,0.0
+
+#calculadora
+masa=30
+aceleracion_centripeta=4
+resultante_centripeta=(masa*aceleracion_centripeta)
+
+#mostrar datos
+print("masa= ", masa)
+print("aceleracion centripeta=", aceleracion_centripeta)
+print("resultante centripeta= ", resultante_centripeta)
+
+#calculadora nro10
+#esta calculadora calcula el empuje del agua
+
+#declaracion de variables
+densidad,volumen_de_una_pelota,gravedad,empuje_del_agua=0.0,0.0,0.0,0.0
+
+#calculadora
+densidad=30
+volumen_de_una_pelota=8
+gravedad=10
+empuje_del_agua=(densidad*gravedad*volumen_de_una_pelota)
+
+#mostrar datos
+print("densidad= ", densidad)
+print("volumen de la pelota= ", volumen_de_una_pelota)
+print("gravedad= ", gravedad)
+print("empuje del agua= ", empuje_del_agua)
+
+#calculadora nro11
+#esta calculadora halla la velocidad final
+
+#declaracion de variables
+velocidad_inicial,aceleracion,tiempo,velocidad_final=0.0,0.0,0.0,0.0
+
+#calculadora
+velocidad_inicial=30
+aceleracion=3
+tiempo=5
+velocidad_final=(velocidad_inicial+aceleracion*tiempo)
+
+#mostrar datos
+print("velocidad inicial= ", velocidad_inicial )
+print("aceleracion= ", aceleracion)
+print("tiempo= ", tiempo)
+print("velocidad final= ",velocidad_final )
+
+#calculadora nro12
+#esta calculadora halla la altura
+
+#declaracion de variables
+velocidad,tiempo,aceleracion,altura=0.0,0.0,0.0,0.0
+
+#calculadora
+velocidad=8
+tiempo=3
+aceleracion=6
+altura=(velocidad*tiempo+(aceleracion*tiempo*tiempo)/2)
+
+#mostrar datos
+print("velocidad= ",velocidad)
+print("tiempo= ", tiempo)
+print("aceleracion= ",aceleracion)
+print("altura= ", altura)
+
+#calculadora nro13
+#esta calculadora halla el area de un circulo
+
+#declaracion de variables
+radio,pi,area_de_un_circulo=0.0,0.0,0.0
+
+#calculadora
+radio=4
+pi=3.14
+area_de_un_circulo=(pi*radio*radio)
+
+#mostrar datos
+print("radio= ", radio)
+print("pi=", pi)
+print("area de un circulo= ", area_de_un_circulo)
+
+#calculadora nro13
+#esta calculadora halla el volumen de un paralepipedo
+
+#declaracion de variables
+ancho,largo,altura,volumen_del_paralepipedo=0.0,0.0,0.0,0.0
+
+#calculadora
+ancho=20
+largo=30
+altura=10
+volumen_del_paralepipedo=(ancho*largo*altura)
+
+#mostrar datos
+print("ancho= ", ancho)
+print("largo= ", largo)
+print("altura= ", altura)
+print("volumen del paralepipedo= ", volumen_del_paralepipedo)
+
+#calculadora nro14
+#esta calculadora halla el volumen del cilindro
+
+#declaracnion de variables
+pi,radio,altura,volumen_del_cilindro=0.0,0.0,0.0,0.0
+
+#calculadora
+pi=3.14
+radio=25
+altura=6
+volumen_del_cilindro=(pi*radio*altura)
+
+#mostrar datos
+print("pi= ", pi)
+print("radio= ", radio)
+print("altura= ", altura)
+print("volumen del cilindro= ", volumen_del_cilindro)
+
+#calculadora nro15
+#esta calculadora halla el area de un trapecio
+
+#declaracion de variables
+base_mayor,base_menor,altura,area_del_trapecio=0.0,0.0,0.0,0.0
+
+#calculadora
+base_mayor=20
+base_menor=10
+altura=8
+area_del_trapecio=(base_mayor+base_menor)*altura/2
+
+#mostar datos
+print("base mayor= ", base_mayor)
+print("base menor= ", base_menor)
+print("altura= ", altura)
+print("area del trapecio= ", area_del_trapecio)
+
+#calculadora nro16
+#esta variable calcula la densidad hidrostatica
+
+#declaracion de variables
+densidad,gravedad,altura,presion_hidrostatica=0.0,0.0,0.0,0.0
+
+#calculadora
+densidad=20
+gravedad=10
+altura=9
+presion_hidrostatica=(densidad*gravedad*altura)
+
+#mostrar
+print("densidad= ", densidad)
+print("gravedad= ", gravedad)
+print("altura= ", altura)
+print("presion hidrostatica= ", presion_hidrostatica)
+
+#calculadora nro17
+#esta calculadora halla la capacitancia de un foco
+
+#declaracion de variables
+carga,voltaje,capacitancia=0.0,0.0,0.0
+
+#calculadora
+carga=50
+voltaje=2
+capacitancia=(carga/voltaje)
+
+#mostrar datos
+print("carga= ", carga)
+print("voltaje= ", voltaje)
+print("capacitancia= ", capacitancia)
+
+#calcula nro18
+#esta calculadora halla los faradios de una linterna
+
+#declaracion de variables
+capacitancia,voltaje,faradeos=0.0,0.0,0.0
+
+#calculadora=
+capacitancia=100
+voltaje=25
+faradeos=(capacitancia/voltaje)
+
+#mostrar datos
+print("capacitancia= ", capacitancia)
+print("voltaje= ", voltaje)
+print("faradeos= ", faradeos)
+
+#calculdora nro19
+#esta calculadora halla la fuerza electrica
+
+#declaracion de variables
+carga,intensidad_de_campo_electrico,fuerza_electrica=0.0,0.0,0.0
+
+#calculadora
+carga=80
+intensidad_de_campo_electrico=10
+fuerza_electrica=(carga*intensidad_de_campo_electrico)
+
+#mostrar datos
+print("carga= ", carga)
+print("intensidad de campo electrico= ", intensidad_de_campo_electrico)
+print("fuerza electrica= ", fuerza_electrica)
+
+#calculadora nro20
+#esta calucladora halla la velocidad tangencial
+
+#declaracion de variables
+velocidad_angular,radio,velocidad_tangencia=0.0,0.0,0.0
+
+#calculadora
+velocidad_angular=80
+radio=30
+velocidad_tangencial=(velocidad_angular*radio)
+
+#mostrar datos
+print("velocidad angular= ", velocidad_angular)
+print("radio= ", radio)
+print("velocidad tangencia= ", velocidad_tangencial)
+
+
+
+
+
